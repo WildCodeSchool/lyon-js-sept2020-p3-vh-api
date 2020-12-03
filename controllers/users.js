edf0d9ac0ca6e405fc17f48b39edaefa37bda61a
Nicolas Dantas <nicolas.dantas@gmail.com>
2020-12-03 16:31:01 +0100

    route users added

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,27 @@
 const {findAll, findOne, createUser, deleteUser, updateUser} = require ('../models/users.js')
 
-module.exports.handleAllUsers = async (req, res) => {
+module.exports.handleAllUsers = async (res) => {
     const rawData = await findAll();
     res.json(rawData)
 }
 
 module.exports.handleAnUser = async (req, res) => {
-    const rawData = await findOne();
+    const rawData = await findOne(req.params.id);
     res.json(rawData)
 }
 
-module.exports.handleOneUserCreation = async (req, res) => {
+module.exports.handleOneUserCreation = async (res) => {
     const rawData = await createUser();
     res.json(rawData)
 }
 
 module.exports.handleOneUserDeletion = async (req, res) => {
-    const rawData = await deleteUser();
+    await deleteUser(req.params.id);
     res.sendStatus(204)
 }
 
 module.exports.handleOneUserUpdate = async (req, res) => {
-    const rawData = await updateUser();
+    const rawData = await updateUser(req.params.id);
     res.json(rawData)
 }
 
--- models/users.js
+++ models/users.js
@@ -6,31 +6,31 @@ const findAll = async () => {
     return db.query('SELECT * FROM user');
 }
 
-const findOne = async () => {
+const findOne = async (req) => {
     const userId = req.params.id
     return db.query('SELECT * FROM user WHERE id=?', [userId]);
 }
 
 
-const createUser = async () => {
+const createUser = async (req) => {
     const { firstname, lastname, email, password, phone_number, bio, photo, website, role, facebook_url, twitter_url, instagram_url } = req.body;
     return db.query('INSERT INTO users(firstname, lastname, email, password, phone_number, bio, photo, website, role, facebook_url, twitter_url, instagram_url) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',  [firstname, lastname, email, password, phone_number, bio, photo, website, role, facebook_url, twitter_url, instagram_url]);
 }
 
-const deleteUser = async () => {
+const deleteUser = async (req) => {
     const userId = req.params.id
     return db.query('DELETE FROM user WHERE id=userId', [userId]);
 }
 
-const updateUser = async () => {
+const updateUser = async (req) => {
     const userId = req.params.id
     if (req.body.role === 'moderator') {
         const { firstname, lastname, email, password, phone_number, bio, photo, website, role, facebook_url, twitter_url, instagram_url } = req.body;
         return db.query('UPDATE user SET firstname=?, lastname=?, email=?, password=?, phone_number=?, bio=?, photo=?, website=?, role=?, facebook_url=?, twitter_url=?, instagram_url=? WHERE id=?',  [firstname, lastname, email, password, phone_number, bio, photo, website, role, facebook_url, twitter_url, instagram_url, userId]);
-    } else {
+    } 
         const { firstname, lastname, email, password, phone_number} = req.body;
         return db.query('UPDATE user SET firstname=?, lastname=?, email=?, password=?, phone_number=? WHERE id=?',  [firstname, lastname, email, password, phone_number, userId]);
-    }
+    
     
 }
 
