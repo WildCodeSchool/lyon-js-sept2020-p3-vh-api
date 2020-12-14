e31c89c3bcc6c29f500e7baaf0d2b5e7579c0940
Nicolas Dantas <nicolas.dantas@gmail.com>
2020-12-14 10:11:01 +0100

    starting docker

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const usersRouter= require('./users');
-const eventsRouter= require('./events');
 
 // eslint-disable-next-line
 module.exports = (app) => {
   app.use('/users', usersRouter);
-  app.use('/events', eventsRouter);
 };
