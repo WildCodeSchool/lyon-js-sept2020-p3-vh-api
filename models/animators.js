23e087ec6328ee0e3ee55962cedb5c6fd9f15236
Aymeric <aymeric.bouault@gmail.com>
2020-12-21 14:32:11 +0100

    mise en place du dossier models pour l'architecture de la route animators

---
+++ models/animators.js
@@ -0,0 +1,48 @@
+const db = require('../db.js');
+const { RecordNotFoundError } = require('../error-types');
+const definedAttributesToSqlSet = require('../helpers/definedAttributesToSQLSet.js');
+
+const getAllAnimators = async () => {
+  return db.query('SELECT * FROM animators')
+};
+
+const getOneAnimator = async (id, failIfNotFound = true) => {
+  const rows = await db.query('SELECT * FROM animators WHERE id = ?', [id]);
+  if (rows.length) {
+    return rows[0];
+  }
+  if (failIfNotFound) throw new RecordNotFoundError();
+  return null;
+};
+
+
+const postOneAnimator = async (formData) => {
+  return db
+    .query(
+      `INSERT INTO animators SET ${definedAttributesToSqlSet(formData)}`,
+      formData
+    )
+    .then((res) => getOneAnimator(res.insertId));
+};
+
+const putOneAnimator = async (id, formData) => {
+    const attribute = definedAttributesToSqlSet(formData);
+    return db
+      .query(`UPDATE animators SET ${attribute} WHERE id = :id`, {
+        ...formData,
+        id,
+      })
+      .then(() => getOneAnimator(id));
+  };
+
+const deleteOneAnimator = async (req) => {
+  const idAnimator = req.params.id
+    return db.query('DELETE FROM animators WHERE id = ?'
+    ,[idAnimator]
+    )
+  };
+
+
+
+
+module.exports = {getAllAnimators,getOneAnimator, postOneAnimator,putOneAnimator, deleteOneAnimator };
