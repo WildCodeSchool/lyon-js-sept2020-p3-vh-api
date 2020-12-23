2a4c902c49c19f198e99e4cbc512f0d3203a312d
Aymeric <aymeric.bouault@gmail.com>
2020-12-23 18:26:44 +0100

    modification route animators

--- models/animators.js
+++ models/animators.js
@@ -3,11 +3,11 @@ const { RecordNotFoundError } = require('../error-types');
 const definedAttributesToSqlSet = require('../helpers/definedAttributesToSQLSet.js');
 
 const getAllAnimators = async () => {
-  return db.query('SELECT * FROM animators')
+  return db.query(`SELECT * FROM user WHERE role='animator'`)
 };
 
 const getOneAnimator = async (id, failIfNotFound = true) => {
-  const rows = await db.query('SELECT * FROM animators WHERE id = ?', [id]);
+  const rows = await db.query('SELECT * FROM user WHERE id = ?', [id]);
   if (rows.length) {
     return rows[0];
   }
@@ -19,7 +19,7 @@ const getOneAnimator = async (id, failIfNotFound = true) => {
 const postOneAnimator = async (formData) => {
   return db
     .query(
-      `INSERT INTO animators SET ${definedAttributesToSqlSet(formData)}`,
+      `INSERT INTO user SET ${definedAttributesToSqlSet(formData)}`,
       formData
     )
     .then((res) => getOneAnimator(res.insertId));
@@ -28,7 +28,7 @@ const postOneAnimator = async (formData) => {
 const putOneAnimator = async (id, formData) => {
     const attribute = definedAttributesToSqlSet(formData);
     return db
-      .query(`UPDATE animators SET ${attribute} WHERE id = :id`, {
+      .query(`UPDATE user SET ${attribute} WHERE id = :id`, {
         ...formData,
         id,
       })
@@ -38,7 +38,7 @@ const putOneAnimator = async (id, formData) => {
 
 const deleteOneAnimator = async (req) => {
   const idAnimator = req.params.id
-    return db.query('DELETE FROM animators WHERE id = ?'
+    return db.query('DELETE FROM user WHERE id = ?'
     ,[idAnimator]
     )
   };
