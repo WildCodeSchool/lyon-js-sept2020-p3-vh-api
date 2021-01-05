58074794d01b233e78cd4bac71a162fb08e99c21
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-05 14:41:53 +0100

    added a new route to find an event by its id

--- controllers/events.js
+++ controllers/events.js
@@ -1,9 +1,13 @@
-const { findAllEvents, createEvent, updateEvent, deleteEvent } = require('../models/events');
+const { findAllEvents, findOneEvent, createEvent, updateEvent, deleteEvent } = require('../models/events');
 
 module.exports.handleAllEvents = async (req, res) => {
  const datas = await findAllEvents();
  res.send(datas.map(({ id, date, moderator_id, main_picture_url, address_id }) => ({id, date, moderator_id, main_picture_url, address_id })));
 };
+
+module.exports.handleAnEvent = async (req, res) => {
+  res.send(await findOneEvent(req.params.id));
+}
 
 module.exports.handleCreateEvent = async (req, res) => {
  const { date, title, price , description, moderator_id, duration_seconds, main_picture_url, address_id } = req.body;
--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,12 @@
 const eventsRouter = require('express').Router();
 const asyncHandler = require('express-async-handler');
-const {handleAllEvents, handleCreateEvent, handleUpdateEvent, handleDeleteEvent} = require('../controllers/events.js');
+const {handleAllEvents, handleAnEvent, handleCreateEvent, handleUpdateEvent, handleDeleteEvent} = require('../controllers/events.js');
 // const requireAdmin = require('../middlewares/requireAdmin.js');
 const requireRequestBody = require('../middlewares/requireRequestBody.js');
 
 
 eventsRouter.get('/', asyncHandler(handleAllEvents));
+eventsRouter.get('/:id', asyncHandler(handleAnEvent));
 eventsRouter.post('/',requireRequestBody, asyncHandler(handleCreateEvent));
 eventsRouter.put('/:id',requireRequestBody,asyncHandler(handleUpdateEvent));
 eventsRouter.delete('/:id', asyncHandler(handleDeleteEvent));
