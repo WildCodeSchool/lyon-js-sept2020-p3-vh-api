65f262df92842306e6b5af86bb5bce1413906ad1
Thibault Verin <thibaultverin@hotmail.com>
2021-01-06 15:40:54 +0100

    Few changes on createEvent method

--- controllers/events.js
+++ controllers/events.js
@@ -1,24 +1,89 @@
-const { findAllEvents, createEvent, updateEvent, deleteEvent } = require('../models/events');
+const {
+  findAllEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} = require('../models/events');
 
 module.exports.handleAllEvents = async (req, res) => {
- const datas = await findAllEvents();
- res.send(datas.map(({ id, date, moderator_id, main_picture_url, address_id }) => ({id, date, moderator_id, main_picture_url, address_id })));
+  const datas = await findAllEvents();
+  res.send(
+    datas.map(
+      ({
+        id,
+        date,
+        moderator_id,
+        main_picture_url,
+        address_id,
+        title,
+        description,
+        price,
+        duration_seconds,
+      }) => ({
+        id,
+        date,
+        moderator_id,
+        main_picture_url,
+        address_id,
+        title,
+        description,
+        price,
+        duration_seconds,
+      })
+    )
+  );
 };
 
 module.exports.handleCreateEvent = async (req, res) => {
- const { date, title, price , description, moderator_id, duration_seconds, main_picture_url, address_id } = req.body;
- const createdUserId = await createEvent({date, title, price , description, moderator_id, duration_seconds, main_picture_url, address_id});
- return res.status(201).json(createdUserId)
-  };
+  const {
+    date,
+    title,
+    price,
+    description,
+    moderator_id,
+    duration_seconds,
+    main_picture_url,
+    address_id,
+  } = req.body;
+  const createdUserId = await createEvent({
+    date,
+    title,
+    price,
+    description,
+    moderator_id,
+    duration_seconds,
+    main_picture_url,
+    address_id,
+  });
+  return res.status(201).json(createdUserId);
+};
 
 module.exports.handleUpdateEvent = async (req, res) => {
- const { date, title, price , description, moderator_id, duration_seconds, main_picture_url, address_id } = req.body;
- const attributes = {date, title, price , description, moderator_id, duration_seconds, main_picture_url, address_id };
- const data = await updateEvent(req.params.id, attributes);
- res.send(data);
+  const {
+    date,
+    title,
+    price,
+    description,
+    moderator_id,
+    duration_seconds,
+    main_picture_url,
+    address_id,
+  } = req.body;
+  const attributes = {
+    date,
+    title,
+    price,
+    description,
+    moderator_id,
+    duration_seconds,
+    main_picture_url,
+    address_id,
+  };
+  const data = await updateEvent(req.params.id, attributes);
+  res.send(data);
 };
 
 module.exports.handleDeleteEvent = async (req, res) => {
   await deleteEvent(req.params.id);
-  return res.sendStatus(204)
+  return res.sendStatus(204);
 };
