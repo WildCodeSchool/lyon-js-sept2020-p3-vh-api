966d27036674754f20a04566166839cb849d0172
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-01-07 15:27:16 +0100

    add faq table and 2 examples

---
+++ migrations/sqls/20210107141140-createFAQTable-up.sql
@@ -0,0 +1,33 @@
+CREATE TABLE faq
+(
+    faq_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    faq_title VARCHAR(255),
+    faq_content TEXT
+);
+INSERT INTO faq (faq_title, faq_content) VALUES ("Qu'est ce que l'hypnose", "L’hypnose est un état de conscience modifiée, proche de l’état dans lequel vous vous trouvez lorsque vous rêvez, ou lorsque vous êtes totalement absorbé dans votre imagination.
+
+
+Comme par exemple, lorsque vous êtes absorbé par un livre qui vous passionne et que vous n’entendez plus rien de ce qui se passe autour de vous…
+
+
+Ou encore, lorsque vous êtes au cinéma et que vous rentrez dans le film jusqu’à en oublier la salle. Vous riez alors aux éclats, vous frissonnez… Vous ressentez et vivez ce qui se passe pour les personnages du film.
+
+
+Ou encore, lorsque vous conduisez votre voiture, et que votre esprit conscient divague et se laisse emporter dans vos rêveries. A ce moment-là, vous avez, en quelque sorte, branché votre « pilote automatique », et d’un coup, vous vous rendez compte que vous êtes déjà arrivé à destination, sans avoir pris conscience du chemin parcouru !
+
+
+Tout comme l’hypnose, ce sont là des états de conscience modifiée, appelés aussi « transes communes de la vie quotidienne »."), ("Comment agit l’hypnose ?", "D’après les travaux du docteur Milton Erickson, le subconscient est « un réservoir d’expériences et de sagesse », […] « un terreau fertile en solutions potentielles pour résoudre nos problèmes ».
+
+
+L’hypnose permet d’accéder au subconscient. A cette partie de notre cerveau, où sont enfouies nos croyances limitantes, mais aussi nos ressources et nos émotions.
+
+
+Elle permet de contourner notre esprit critique. Cette partie de nous qui dit si souvent : «ça ne marchera pas, je ne suis pas capable, j’ai peur, ce n’est pas dans ma nature, je suis bien trop…»
+
+Ainsi, l’hypnose permet de court-circuiter notre mental et nos blocages. Elle agit en profondeur et stimule nos ressources positives.
+
+
+L’hypnothérapeute occupe le rôle d’accompagnant. Il permet d’accéder à cet inconscient, où, comme dans un rêve, tout devient possible.
+
+
+La clé de cette pratique repose sur une participation active du client. Car celui-ci possède les ressources nécessaires pour répondre de manière innovante aux situations qu’il rencontre. Ainsi, il effectue en douceur les transformations intérieures désirées.");
