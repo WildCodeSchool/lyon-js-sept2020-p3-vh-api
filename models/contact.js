8f729730f14c69e140e3949f812d0399cafca592
LaurencePortron <portron.laurence@gmail.com>
2021-01-25 09:10:49 +0100

    merge dev

--- models/contact.js
+++ models/contact.js
@@ -1,56 +1,84 @@
 const Joi = require('joi');
-const SibApiV3Sdk = require("sib-api-v3-sdk");
+const SibApiV3Sdk = require('sib-api-v3-sdk');
 const db = require('../db.js');
 const { RecordNotFoundError } = require('../error-types');
 const definedAttributesToSqlSet = require('../helpers/definedAttributesToSQLSet.js');
-const {ValidationError} = require('../error-types')
-const {SENDINBLUE_API_KEY} = require('../env');
+const { ValidationError } = require('../error-types');
+const { SENDINBLUE_API_KEY } = require('../env');
 
 const validate = (datas) => {
   const schema = Joi.object().keys({
-    firstname: Joi.string().min(1).max(70).required().regex(/^[a-z ,.'-]+$/i).messages({ 'string.min':"Merci d'indiquer votre prénom",'string.max':'Votre prénom ne doit pas excéder 70 caractères', 'string.pattern.base' :'Votre prénom contient des caractères non autorisés' }),
-    lastname: Joi.string().min(1).max(70).required().regex(/^[a-z ,.'-]+$/i).messages({ 'string.min':"Merci d'indiquer votre nom",'string.max':'Votre nom ne doit pas excéder 70 caractères', 'string.pattern.base' :'Votre nom contient des caractères non autorisés' }),
-    email: Joi.string().email().required().messages({ 'required':"Merci d'indiquer votre email", 'string.email':"Votre email n'est pas valide" }),
-    message: Joi.string().required().messages({ 'required':"Merci de saisir un message", }),
-    purpose: Joi.string().required().messages({ 'required':"Merci de choisir un sujet", }),
+    firstname: Joi.string()
+      .min(1)
+      .max(70)
+      .required()
+      .regex(/^[a-z ,.'-]+$/i)
+      .messages({
+        'string.min': "Merci d'indiquer votre prénom",
+        'string.max': 'Votre prénom ne doit pas excéder 70 caractères',
+        'string.pattern.base':
+          'Votre prénom contient des caractères non autorisés',
+      }),
+    lastname: Joi.string()
+      .min(1)
+      .max(70)
+      .required()
+      .regex(/^[a-z ,.'-]+$/i)
+      .messages({
+        'string.min': "Merci d'indiquer votre nom",
+        'string.max': 'Votre nom ne doit pas excéder 70 caractères',
+        'string.pattern.base':
+          'Votre nom contient des caractères non autorisés',
+      }),
+    email: Joi.string().email().required().messages({
+      required: "Merci d'indiquer votre email",
+      'string.email': "Votre email n'est pas valide",
+    }),
+    message: Joi.string()
+      .required()
+      .messages({ required: 'Merci de saisir un message' }),
+    purpose: Joi.string()
+      .required()
+      .messages({ required: 'Merci de choisir un sujet' }),
   });
   const { error } = schema.validate(datas, {
     abortEarly: false,
   });
-  if (error) throw new ValidationError([
-    { message: error.details.map(err => err.message), path: ['joi'], type: 'unique' }
-  ]);
-}
+  if (error)
+    throw new ValidationError([
+      {
+        message: error.details.map((err) => err.message),
+        path: ['joi'],
+        type: 'unique',
+      },
+    ]);
+};
 
-  const sendMail = (datas) => {
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
-    const apiKey = defaultClient.authentications["api-key"];
-    apiKey.apiKey = SENDINBLUE_API_KEY;
-    
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+const sendMail = (datas) => {
+  const defaultClient = SibApiV3Sdk.ApiClient.instance;
+  const apiKey = defaultClient.authentications['api-key'];
+  apiKey.apiKey = SENDINBLUE_API_KEY;
 
-    const {firstname, lastname, email, message, purpose} = datas;
-    sendSmtpEmail.subject = "Vous avez un nouveau message";
-    sendSmtpEmail.htmlContent =
-      `<html><body><h1>Vous avez reçu un nouveau message de ${firstname} ${lastname}</h1><p>Mail de l'expéditeur : ${email}</p><p>Sujet : ${purpose}</p><p>Message : ${message}</p></body></html>`;
-    sendSmtpEmail.sender = { name: `${firstname} ${lastname}`, email };
-    sendSmtpEmail.to = [{ email: "coronavirus.france2019@gmail.com" }];
-    sendSmtpEmail.replyTo = { email, name: `${firstname} ${lastname}` };
+  const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+  const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
 
-    try {
-      apiInstance.sendTransacEmail(sendSmtpEmail)
-      return
-    } 
-    catch (err) {
-      console.error(err);
-    }
-  };
-  
+  const { firstname, lastname, email, message, purpose } = datas;
+  sendSmtpEmail.subject = 'Vous avez un nouveau message';
+  sendSmtpEmail.htmlContent = `<html><body><h1>Vous avez reçu un nouveau message de ${firstname} ${lastname}</h1><p>Mail de l'expéditeur : ${email}</p><p>Sujet : ${purpose}</p><p>Message : ${message}</p></body></html>`;
+  sendSmtpEmail.sender = { name: `${firstname} ${lastname}`, email };
+  sendSmtpEmail.to = [{ email: 'coronavirus.france2019@gmail.com' }];
+  sendSmtpEmail.replyTo = { email, name: `${firstname} ${lastname}` };
 
+  try {
+    apiInstance.sendTransacEmail(sendSmtpEmail);
+    return;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const getAllMessages = async () => {
-  return db.query('SELECT * FROM messages')
+  return db.query('SELECT * FROM messages');
 };
 
 const getOneMessage = async (id, failIfNotFound = true) => {
@@ -62,10 +90,9 @@ const getOneMessage = async (id, failIfNotFound = true) => {
   return null;
 };
 
-
 const postOneMessage = async (formData) => {
-  await validate(formData)
-  await sendMail(formData)
+  await validate(formData);
+  await sendMail(formData);
   return db
     .query(
       `INSERT INTO messages SET ${definedAttributesToSqlSet(formData)}`,
@@ -75,13 +102,13 @@ const postOneMessage = async (formData) => {
 };
 
 const deleteOneMessage = async (req) => {
-  const idMessage = req.params.id
-    return db.query('DELETE FROM messages WHERE id = ?'
-    ,[idMessage]
-    )
-  };
+  const idMessage = req.params.id;
+  return db.query('DELETE FROM messages WHERE id = ?', [idMessage]);
+};
 
-
-
-
-module.exports = {getAllMessages,getOneMessage, postOneMessage, deleteOneMessage };
+module.exports = {
+  getAllMessages,
+  getOneMessage,
+  postOneMessage,
+  deleteOneMessage,
+};
