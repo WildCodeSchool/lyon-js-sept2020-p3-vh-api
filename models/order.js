a8d5ba999627e35793c0625f71784863603d1c40
LaurencePortron <portron.laurence@gmail.com>
2021-01-25 12:06:58 +0100

    added date to order/user

--- models/order.js
+++ models/order.js
@@ -1,24 +1,24 @@
-const { v4: uuidv4 } = require("uuid");
-const db = require("../db.js");
-const { RecordNotFoundError } = require("../error-types");
-const definedAttributesToSqlSet = require("../helpers/definedAttributesToSQLSet.js");
+const { v4: uuidv4 } = require('uuid');
+const db = require('../db.js');
+const { RecordNotFoundError } = require('../error-types');
+const definedAttributesToSqlSet = require('../helpers/definedAttributesToSQLSet.js');
 
 const getAllOrders = async () => {
   return db.query(
-    "SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id"
+    'SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email,  e.date, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id'
   );
 };
 
 const findOneOrder = async (id, failIfNotFound = true) => {
   const orderId = id;
   const rows = await db.query(
-    "SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE order_id=?",
+    'SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE order_id=?',
     [orderId]
   );
   if (rows.length) {
     return rows;
   }
-  if (failIfNotFound) throw new RecordNotFoundError("event", orderId);
+  if (failIfNotFound) throw new RecordNotFoundError('event', orderId);
   return null;
 };
 
@@ -53,34 +53,34 @@ const postOneOrder = async (req) => {
 
 const findOrdersByUser = async (id, failIfNotFound = true) => {
   const rows = await db.query(
-    "SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE u.id = ?",
+    'SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.date, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE u.id = ?',
     [id]
   );
   if (rows.length) {
     return rows;
   }
-  if (failIfNotFound) throw new RecordNotFoundError("event", id);
+  if (failIfNotFound) throw new RecordNotFoundError('event', id);
   return null;
 };
 
 const findOrdersByEvent = async (id, failIfNotFound = true) => {
   const rows = await db.query(
-    "SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE e.id = ?",
+    'SELECT o.order_id, u.id as user_id, u.firstname, u.lastname, u.email, e.id as event_id, o.event_quantity, e.title, e.price  FROM `order` as o JOIN user AS u ON o.user_id = u.id JOIN event as e ON o.event_id = e.id WHERE e.id = ?',
     [id]
   );
   if (rows.length) {
     return rows;
   }
-  if (failIfNotFound) throw new RecordNotFoundError("event", id);
+  if (failIfNotFound) throw new RecordNotFoundError('event', id);
   return null;
 };
 
 const deleteOneOrder = async (id, failIfNotFound = true) => {
-  const res = await db.query("DELETE FROM `order` WHERE order_id=?", [id]);
+  const res = await db.query('DELETE FROM `order` WHERE order_id=?', [id]);
   if (res.affectedRows !== 0) {
     return true;
   }
-  if (failIfNotFound) throw new RecordNotFoundError("event", id);
+  if (failIfNotFound) throw new RecordNotFoundError('event', id);
   return false;
 };
 
